957d36df9942304b87cc55c9f972d8fb5f0a2a1c
Steven Nagie <sanagie@yahoo.com>
2017-01-10 15:57:51 -0700

    Node/Express and Gulp all working correctly. All that's left to do is fill in my information.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var session = require('express-session');
+var config = require('./server_config.json');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(session({
+  secret: config.sessionSecret,
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.listen(config.port, function() {
+  console.log('listening on port ' + config.port);
+})
